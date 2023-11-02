f77585a2e5a78fc43d3688d3572fbc213a24666f
Greg Walker <git@greg.email>
2023-11-02 17:11:42 -0500

    fix some arrived bugs and some last-station ones too

--- src/site/helpers/stationStatusText.js
+++ src/site/helpers/stationStatusText.js
@@ -41,11 +41,48 @@ export const stationStatusText = function () {
 
     stringParts.push("<ul><li><strong>Expected to arrive</strong> ");
     stringParts.push(dateAndTimeString(arrival));
-    stringParts.push("</li><li><strong>Expected to depart</strong>");
-    stringParts.push(dateAndTimeString(departure));
-    stringParts.push("</li></ul>");
+
+    // If it's the last train, there's no departure. Dun-dun.
+    if (this.departureScheduled) {
+      stringParts.push("</li><li><strong>Expected to depart</strong>");
+      stringParts.push(dateAndTimeString(departure));
+      stringParts.push("</li></ul>");
+    }
   } else if (this.status === "arrived") {
     // train is here
+    const arrived = dayjs(this.arrivalActual).tz(this.timezone);
+    const scheduled = dayjs(this.arrivalScheduled).tz(this.timezone);
+    const delay = dayjs.duration(arrived.diff(scheduled));
+
+    stringParts.push("<li><strong>Arrived</strong> ");
+    stringParts.push(dateAndTimeString(arrived));
+    stringParts.push("</li>");
+
+    if (this.departureScheduled) {
+      const departure = dayjs(
+        this.departureEstimated ?? this.departureScheduled,
+      ).tz(this.timezone);
+
+      stringParts.push("<li><strong>Expected to depart</strong>");
+      stringParts.push(dateAndTimeString(departure));
+      stringParts.push("</li>");
+    }
+    stringParts.push("</ul>");
+
+    if (delay.asMinutes() > 10) {
+      // If this is the last station, then we don't have to expect how late the
+      // train will be. It has finished its route and we know how late it
+      // actually was.
+      if (!this.departureScheduled) {
+        stringParts.push("<p>This train was about about ");
+        stringParts.push(delay.humanize());
+        stringParts.push(" late.</p>");
+      } else {
+        stringParts.push("<p>This train is expected to be about ");
+        stringParts.push(delay.humanize());
+        stringParts.push(" late.</p>");
+      }
+    }
   }
 
   return new handlebars.SafeString(stringParts.join(""));
