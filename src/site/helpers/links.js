2a7d2e49dde175e584c411d52858054342f3924c
Greg Walker <git@greg.email>
2023-11-02 21:34:17 -0500

    fix links on route pages

--- src/site/helpers/links.js
+++ src/site/helpers/links.js
@@ -4,6 +4,11 @@ export const routeLink = function () {
   return `${slugify(this.route)}/`;
 };
 
-export const trainLink = function () {
-  return `${slugify(this.route)}/${this.number}-${this.id}.html`;
+export const trainLink = function (isIndexPage) {
+  const url = `${this.number}-${this.id}.html`;
+
+  if (isIndexPage) {
+    return `${slugify(this.route)}/${url}`;
+  }
+  return url;
 };
