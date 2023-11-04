735e9f1c7ef68d2f6acf0f01b54e5e4018a75ae6
Greg Walker <git@greg.email>
2023-11-04 13:58:45 -0500

    make the data what I want instead of using helpers

--- src/site/build.js
+++ src/site/build.js
@@ -1,18 +1,193 @@
+import dotenv from "dotenv";
 import fs from "node:fs/promises";
 import path from "node:path";
 import handlebars from "handlebars";
-import * as helpers from "./helpers/index.js";
-import { slugify } from "./utils.js";
+import { dayjs, slugify } from "./utils.js";
+
+dotenv.config();
+
+const dayAndTime = (str, tz) => {
+  const ts = dayjs(str).tz(tz);
+  return `${ts.format("dddd")} at ${ts.format("h:mm A")}`;
+};
+
+const BASE_URL = process.env.BASE_URL ?? "/amtrak-api";
+
+const TAG_BG_COLORS = new Map([
+  ["scheduled", "bg-base-lighter"],
+  ["departed", "bg-primary-dark"],
+  ["enroute", "bg-green"],
+  ["on time", "bg-green"],
+  ["arrived", "bg-gold"],
+]);
+
+const TAG_TEXT_COLORS = new Map([
+  ["scheduled", "text-black"],
+  ["departed", "text-white"],
+  ["enroute", "text-white"],
+  ["on time", "text-white"],
+  ["arrived", "text-black"],
+]);
+
+const STATION_COLORS = new Map([
+  ["arrived", "bg-gold"],
+  ["enroute", "bg-green"],
+  ["departed", "bg-primary-dark"],
+  ["scheduled", "bg-base-light"],
+]);
 
 // Get the route data
 const routes = JSON.parse(
   await fs.readFile("_site/routes.json", { encoding: "utf-8" }),
-);
+).map((routeInfo) => {
+  const routeUrl = `${BASE_URL}/${slugify(routeInfo.route)}`;
+  const routePath = `_site/${slugify(routeInfo.route)}`;
+  const route = {
+    name: routeInfo.route,
+    filepath: routePath,
+    url: routeUrl,
+  };
 
-// Register all of our handlebar helpers. Using these helpers feels kinda janky
-// but it does work so... okay.
-Object.entries(helpers).forEach(([name, handler]) => {
-  handlebars.registerHelper(name, handler);
+  route.trains = routeInfo.trains.map((trainInfo) => {
+    const train = {
+      route: route.name,
+      number: trainInfo.number,
+      filepath: `${routePath}/${trainInfo.number}-${trainInfo.id}.html`,
+      url: `${routeUrl}/${trainInfo.number}-${trainInfo.id}.html`,
+      info: "",
+      status: "scheduled",
+      trackColor: "bg-base-light",
+    };
+
+    train.stations = trainInfo.stations.map((stationInfo) => {
+      const station = {
+        name: stationInfo.station.name,
+        code: stationInfo.code,
+        status: stationInfo.status,
+
+        // Are our arrival and departure times known or expected? We know the
+        // arrival time if the train has arrived or departed, and we know the
+        // departure time if the train has departed. In all other cases, we're
+        // just hoping for the best.
+        arrivalKnown:
+          stationInfo.status === "arrived" || stationInfo.status === "departed",
+        departureKnown: stationInfo.status === "departed",
+
+        tag: {
+          text: stationInfo.status,
+          bg: TAG_BG_COLORS.get(stationInfo.status),
+          color: TAG_TEXT_COLORS.get(stationInfo.status),
+        },
+
+        dotColor: STATION_COLORS.get(stationInfo.status),
+        spacerColors: { before: "", after: "" },
+
+        arrival: (() => {
+          const t =
+            stationInfo.arrivalActual ??
+            stationInfo.arrivalEstimated ??
+            stationInfo.arrivalScheduled ??
+            false;
+          if (t) {
+            return dayAndTime(t);
+          }
+          return false;
+        })(),
+        departure: (() => {
+          const t =
+            stationInfo.departureActual ??
+            stationInfo.departureEstimated ??
+            stationInfo.departureScheduled ??
+            false;
+          if (t) {
+            return dayAndTime(t);
+          }
+          return false;
+        })(),
+      };
+
+      return station;
+    });
+
+    train.stations.forEach((station, i) => {
+      if (station.status === "departed") {
+        station.spacerColors.after = "bg-primary-dark";
+      }
+      if (i > 0) {
+        station.spacerColors.before = train.stations[i - 1].spacerColors.after;
+      }
+    });
+
+    if (train.stations[0].status !== "scheduled") {
+      train.status = "on time";
+    }
+    if (train.stations.slice(-1).pop().status === "arrived") {
+      train.status = "arrived";
+    }
+    train.tag = {
+      text: train.status,
+      bg: TAG_BG_COLORS.get(train.status),
+      color: TAG_TEXT_COLORS.get(train.status),
+    };
+    if (train.status !== "scheduled") {
+      train.trackColor = "bg-primary-light";
+    }
+
+    const first = train.stations[0];
+    const final = train.stations.slice(-1).pop();
+
+    if (train.status === "scheduled") {
+      train.info = `Scheduled to depart ${first.name} on ${first.departure} and arrive at ${final.name} on ${final.arrival}.`;
+    } else {
+      const { previous, next } = (() => {
+        const index = train.stations.findLastIndex(
+          ({ status }) => status === "departed" || status === "arrived",
+        );
+        if (index > 0) {
+          return {
+            previous: train.stations[index],
+            next: train.stations[index + 1],
+          };
+        }
+        return {
+          previous: first,
+          next: final,
+        };
+      })();
+
+      const info = [
+        previous.status === "arrived" ? "Arrived at " : "Departed from ",
+        previous.name,
+        " on ",
+        previous.arrival,
+      ];
+
+      if (previous.status === "departed") {
+        info.push(
+          " and is scheduled to arrive at ",
+          next.name,
+          " on ",
+          next.arrival,
+        );
+      } else if (next) {
+        info.push(
+          ". Expected to depart on ",
+          previous.departure,
+          " and arrive at ",
+          next.name,
+          " on ",
+          next.arrival,
+        );
+      }
+      info.push(".");
+
+      train.info = info.join("");
+    }
+
+    return train;
+  });
+
+  return route;
 });
 
 // Register all our handlebar partials so they're available in our templates.
@@ -54,24 +229,17 @@ await fs.writeFile("_site/index.html", indexPage);
 
 // Build all the route pages.
 const routePages = routes.map(async (route) => {
-  // Get the route slug
-  const slug = slugify(route.route);
-
   // Make a directory for the route
-  const dir = path.join("_site", slug);
-  await fs.mkdir(dir, { recursive: true });
+  await fs.mkdir(route.filepath, { recursive: true });
 
   // Build the route index page and write it out.
   const page = indexTemplate({ routes: [route] });
-  await fs.writeFile(path.join(dir, "index.html"), page);
+  await fs.writeFile(path.join(route.filepath, "index.html"), page);
 
   // Now create pages for each train on the route.
   const trainPages = route.trains.map(async (train) => {
     const page = trainTemplate(train);
-    await fs.writeFile(
-      path.join(dir, `${train.number}-${train.id}.html`),
-      page,
-    );
+    await fs.writeFile(path.join(train.filepath), page);
   });
   await Promise.all(trainPages);
 });
