dd67e3b4d52e486ec19c1e9d08570bcdc26703b2
Greg Walker <git@greg.email>
2024-01-31 18:42:20 -0600

    update dependencies; add route-specific endpoints

--- src/data/main.js
+++ src/data/main.js
@@ -2,6 +2,14 @@ import defaultFs from "node:fs/promises";
 import { getStations as defaultGetStations } from "./stations.js";
 import { getTrains as defaultGetTrains } from "./trains.js";
 
+const slugify = (str) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[\s_]/g, "-")
+    .replace(/[^a-z0-9-_]/g, "")
+    .replace(/-{2,}/g, "-");
+
 export const main = async ({
   fs = defaultFs,
   getStations = defaultGetStations,
@@ -9,6 +17,8 @@ export const main = async ({
 } = {}) => {
   const stations = await getStations();
   const trains = await getTrains(stations);
+
+  await fs.mkdir(`_site/routes`, { recursive: true });
 
   await fs.writeFile("_site/stations.json", JSON.stringify(stations));
   await fs.writeFile("_site/trains.json", JSON.stringify(trains));
@@ -29,6 +39,13 @@ export const main = async ({
     });
 
   await fs.writeFile("_site/routes.json", JSON.stringify(routes));
+
+  for await (const route of routes) {
+    await fs.writeFile(
+      `_site/routes/${slugify(route.route)}.json`,
+      JSON.stringify(route),
+    );
+  }
 };
 
 const isMainModule = import.meta.url.endsWith(process.argv[1]);
